a434a29db8a3e9c0ca4106fcce9f5a5295fff596
H Gulliver <harry.gulliver@multiverse.io>
2021-10-22 18:10:23 +0100

    added print statements to .py

--- wtf_acf.py
+++ wtf_acf.py
@@ -9,7 +9,7 @@ bus = pd.read_csv('cleaned_bus.csv', index_col='Month', parse_dates=True)
 
 # compute first 12 lags of ACF with statsmodels
 sm_acf = sm.tsa.acf(bus, nlags=12, fft=False)
-sm_acf
+print(sm_acf)
 
 # compute ACF by shifting and using scipy.stats.pearsonr
 # drop nulls that arise from shifting and corresponding rows of unshifted data
@@ -20,10 +20,10 @@ for lag in range(13):
     corr = pearsonr(shifted, trimmed)[0] # [0] to grab r ([1] is p-value)
     trimmed_acf.append(corr)
 trimmed_acf = np.array(trimmed_acf)
-trimmed_acf
+print(trimmed_acf)
 
 # not the same - how different are they?
-sm_acf - trimmed_acf
+print(sm_acf - trimmed_acf)
 
 # maybe acf is filling in the missing values with zeroes?
 # from looking at the source code, doesn't seem like it, but let's try
@@ -33,9 +33,9 @@ for lag in range(13):
     corr = pearsonr(shifted, bus.riders)[0]
     zeroed_acf.append(corr)
 zeroed_acf = np.array(zeroed_acf)
-zeroed_acf
+print(zeroed_acf)
 
 # different again!
-sm_acf - zeroed_acf
+print(sm_acf - zeroed_acf)
 
 # so why does statsmodels give a different ACF than calculating directly with Pearson's r?
